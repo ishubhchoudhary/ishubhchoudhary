e6db396d1d8884ba43924e0427c47cf2ef969f1b
ishubhchoudhary <76101441+ishubhchoudhary@users.noreply.github.com>
2022-08-02 17:59:01 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,90 @@
+# Shubham Choudhary
+**Vill. Dakachya, indore 452020** <br/>
+```8962887881```  <br/>
+**Email**  shubhamchoudharys.in@gmail.com
+
+---
+
+### CAREER OBJECTIVE
+To use my skills in the best possible way for achieving the company’s goals.
+
+---
+
+### EDUCATION
+
+Degree | Year | Status
+------ | ---- | ------ 
+12th   | 2013 | Completed
+DCA    | 2015 | Completed
+BCA    | 2021 | Completed
+
+
+### TECHNICAL SKILLS
+
+- HTML
+- CSS
+- JAVASCRIPT
+- PHP
+- JQUERY
+- BOOTSTRAP
+
+### WORKING EXPRIENCE
+
+- Flutter
+- Android Native
+- Laravel (API Development Only)
+
+> Experience on `Flutter`
+> * Dio and Http (Api calling)
+> * shared_preferences and getstorage (Session storage)
+> * Provider (State management)
+> * GetX (State management)
+> * Graph (Data analysis)
+> * Razorpay (Payment gateway)
+> * Dynamic link **Using Firebase**
+> * Google, Facebook Login (Authentication)
+> * FCM Notification (push notification)
+>
+> `Product`
+> ---
+> Globegust [Link](https://play.google.com/store/apps/details?id=com.globegust.globe)  <br/>
+> Airbound (Dance Classes app)  <br/>
+> KVB (Restaurant app)  <br/>
+
+
+> Experience on `Android Navite`
+> * Data Binding
+> * retrofit and volley (Api calling)
+> * dagger hilt (dependency injection)
+> * socket io (2 way communication)
+> * Google, Facebook Login (Authentication)
+> * FCM Notification (push notification)
+>
+> `Product`
+> ---
+> Mishra ji Technical [Link](https://play.google.com/store/apps/details?id=com.mjtedu.mishrajitechnical)  <br/>
+> Crictak [Link](https://play.google.com/store/apps/details?id=com.crikar.crictak)  <br/>
+> Crikar [Link](https://play.google.com/store/apps/details?id=com.technomancy.crikar)  <br/>
+
+> Experience on `Laravel`
+> * Laravel Sanctum (Authentication)
+> * Create Api with MySql (DATABASE)
+> * Mailing (using Gmail)
+> * Complete crud operation
+> * Like/comment menegment
+> * Add to cart
+>
+> `Product`
+> ---
+> * work on Crikar Api
+
+---
+
+### Personal Details <br/>
+
+**Name :**               Shubham Choudhary  <br/>
+**Date of Birth :**      20 May 1999  <br/>
+**Father’s Name :**      Sunil Choudhary  <br/>
+**Strength :**           Long Time work, Discipline, Hard worker, Responsibility, Punctuality  <br/>
+**Hobbies :**            Listening music <br/>
+**Permanent Address :**  Vill. Dakachya,indore 453771 <br/>
